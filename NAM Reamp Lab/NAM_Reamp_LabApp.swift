39e57a50439d3ba613f4629d554252679dab5833
Mitchell Cohen <122713703+profmitchell@users.noreply.github.com>
2026-01-22 14:22:23 -0500

    Initial Commit

---
+++ NAM Reamp Lab/NAM_Reamp_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  NAM_Reamp_LabApp.swift
+//  NAM Reamp Lab
+//
+//  Created by Mitchell Cohen on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct NAM_Reamp_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
